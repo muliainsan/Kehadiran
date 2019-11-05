52ef83b0950b0dab893e7938c4ce09606608e97a
insanmulia <insanmuliaisa@gmail.com>
2019-11-05 21:13:27 +0700

    Using Tabbar and fragment

---
+++ app/src/main/java/com/project/insan/kehadiran/MovieFragment.java
@@ -0,0 +1,38 @@
+package com.project.insan.kehadiran;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MovieFragment extends Fragment implements View.OnClickListener{
+
+
+    public MovieFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
