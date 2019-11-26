33648f3707484b2e0381b47c5edc688891a55ab6
insanmulia <insanmuliaisa@gmail.com>
2019-11-27 00:08:08 +0700

    Bottom Navigation

--- app/src/main/java/com/project/insan/kehadiran/activity/MainActivity.java
+++ app/src/main/java/com/project/insan/kehadiran/activity/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.RequiresApi;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -34,7 +35,8 @@ public class MainActivity extends AppCompatActivity{
     private MovieFragment movie_fragment = new MovieFragment();
     private TvFragment tv_fragment =new TvFragment();
     private BottomNavigationView bottomNavigationView;
-    FragmentManager manager;
+    private FragmentManager manager;
+    Fragment active= movie_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,47 +46,58 @@ public class MainActivity extends AppCompatActivity{
         toolbar.inflateMenu(R.menu.toolbar);
 
         bottomNavigationView =  findViewById(R.id.bottom_nav);
+//        viewPager = findViewById(R.id.viewPager);
+//        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//        adapter = new TabAdapter(getSupportFragmentManager());
+//        manager = getSupportFragmentManager();
+//        adapter.addFragment(movie_fragment, getResources().getString(R.string.tab_movie));
+//        adapter.addFragment(tv_fragment, getResources().getString(R.string.tab_tv));
+//        viewPager.setAdapter(adapter);
+//        tabLayout.setupWithViewPager(viewPager);
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                if(item.getItemId()==R.id.english)
+//                {
+//                    Intent intent = new Intent(MainActivity.this, Language.class);
+//                    startActivity(intent);
+//                }
+//                return false;
+//            }
+//        });
+
+//        manager.beginTransaction().add(R.id.fragment, movie_fragment, "Movie").hide(movie_fragment).commit();
+//        manager.beginTransaction().add(R.id.fragment, tv_fragment, "Tv").hide(tv_fragment).commit();
+//        manager.beginTransaction().add(R.id.fragment, movie_fragment, "Favorit").hide(movie_fragment).commit();
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.movie:
-                        Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
+//                        manager.beginTransaction().hide(active).show(movie_fragment).commit();
+                        active = movie_fragment;
                         break;
                     case R.id.tv:
-                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+//                        manager.beginTransaction().hide(active).show(tv_fragment).commit();
+                        active = tv_fragment;
                         break;
                     case R.id.favorite:
-                        Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Favorite", Toast.LENGTH_SHORT).show();
                         break;
                 }
-                return true;
+                return loadFragment(active);
             }
         });
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        adapter = new TabAdapter(getSupportFragmentManager());
-        manager = getSupportFragmentManager();
-        Log.i("cvcv","luar");
-        adapter.addFragment(movie_fragment, getResources().getString(R.string.tab_movie));
-        adapter.addFragment(tv_fragment, getResources().getString(R.string.tab_tv));
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
-
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId()==R.id.english)
-                {
-                    Intent intent = new Intent(MainActivity.this, Language.class);
-                    startActivity(intent);
-                }
-                return false;
-            }
-        });
-
     }
 
-
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
